99fe93c790a1f341003485952a6f33cdd9398fbd
Rahul <rahulghosh@duck.com>
2023-04-16 19:36:36 +0530

    Add JPA persistence to Tacobell

--- src/main/java/com/prac/spring/Model/Ingredient.java
+++ src/main/java/com/prac/spring/Model/Ingredient.java
@@ -1,15 +1,12 @@
 package com.prac.spring.Model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 
 // @Data
 // public class Ingredient {
@@ -23,10 +20,10 @@ import lombok.NoArgsConstructor;
 // }
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-public class Ingredient implements Persistable<String> {
+public class Ingredient {
 
     @Id
     private String id;
@@ -34,10 +31,10 @@ public class Ingredient implements Persistable<String> {
     private String name;
     private Type type;
 
-    @Override
-    public boolean isNew() {
-        return true;
-    }
+    // @Override
+    // public boolean isNew() {
+    //     return true;
+    // }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
--- src/main/java/com/prac/spring/Model/Taco.java
+++ src/main/java/com/prac/spring/Model/Taco.java
@@ -22,16 +22,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@Table
+@Entity
 // Exclude createdAt from equals() method so that tests won't fail trying to
 // compare java.util.Date with java.sql.Timestamp (even though they're
 // essentially
@@ -42,6 +44,7 @@ import lombok.EqualsAndHashCode;
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt = new Date();
@@ -51,10 +54,11 @@ public class Taco {
     private String name;
 
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients = new ArrayList<>();
+    @ManyToMany()
+    private List<Ingredient> ingredients = new ArrayList<>();
 
-    public void addIngredient(Ingredient taco) {
-        this.ingredients.add(new IngredientRef(taco.getId()));
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
     }
 
 }
