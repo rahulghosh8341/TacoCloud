7434a335aa3435b1a9173e0b1ca75e0752e9fbe1
Rahul <rahulghosh@duck.com>
2023-04-22 11:47:00 +0530

    Add logout feature

--- src/main/java/com/prac/spring/Security/SecurityConfig.java
+++ src/main/java/com/prac/spring/Security/SecurityConfig.java
@@ -42,14 +42,12 @@ public class SecurityConfig {
     // return new InMemoryUserDetailsManager(usersList);
     // }
 
-    @Autowired
-    private UserDetailsService userDetailsService;
 
     @Bean
     public UserDetailsService userDetailsService() {
         return new UserRepositoryUserDetailsService();
     }
-
+// 59D23FC70D196AAE41C75B97F8577725
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -57,14 +55,15 @@ public class SecurityConfig {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers(antMatcher("/h2-console/**"), antMatcher("/register")).permitAll()
-                .requestMatchers("/", "/**").permitAll()
-                .requestMatchers("/designjdbc", "/ordersjdbc","/design","/orders").authenticated()
+                .requestMatchers("/designjdbc","/orderjdbc/**").authenticated()
                 .and()
-                .formLogin().loginPage("/login").defaultSuccessUrl("/designjdbc",true)
+                .authorizeHttpRequests()
+                .requestMatchers("/h2-console/**", "/register", "/error").permitAll()
+                .requestMatchers("/", "/**").permitAll()
+                .and()
+                .formLogin().defaultSuccessUrl("/designjdbc",true)
                 .and()
                 .logout()
-                .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID");
         return http.build();
